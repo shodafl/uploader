479822a857d4171f858291c9ef6c2895c54c79a4
shoda-fl <shoda.fl@gmail.com>
2024-08-19 12:42:35 +0300

    Initial commit

---
+++ lib/uploader.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:googleapis/androidpublisher/v3.dart';
+
+Future<void> main(List<String> arguments) async {
+  const scopes = ['https://www.googleapis.com/auth/androidpublisher'];
+
+  if (arguments.length != 3) {
+    throw ArgumentError(
+      'You should pass arguments in order: [packageName] [bundleFilePath] [jsonKeyFilePath]',
+    );
+  }
+
+  final packageName = arguments[0];
+  final bundleFilePath = arguments[1];
+  final jsonKeyFilePath = arguments[2];
+
+  try {
+    final jsonKey = File(jsonKeyFilePath).readAsStringSync();
+    final accountCredentials = ServiceAccountCredentials.fromJson(jsonKey);
+
+    final authClient = await clientViaServiceAccount(
+      accountCredentials,
+      scopes,
+    );
+
+    final appPublisher = AndroidPublisherApi(authClient);
+
+    print('Started creating edit');
+
+    final edit = await appPublisher.edits.insert(AppEdit(), packageName);
+    final editId = edit.id!;
+
+    print('Finished creating edit');
+
+    final file = File.fromUri(Uri.parse(bundleFilePath));
+    final stream = file.openRead();
+    final length = await file.length();
+
+    print('Started uploading bundle');
+
+    await appPublisher.edits.bundles.upload(
+      packageName,
+      editId,
+      uploadMedia: Media(stream, length),
+    );
+
+    print('Finished uploading bundle');
+    print('Started commiting edit');
+
+    await appPublisher.edits.commit(packageName, editId);
+    print('Finished commiting edit');
+
+    exit(0);
+  } catch (exc) {
+    print(exc);
+    exit(1);
+  }
+}
